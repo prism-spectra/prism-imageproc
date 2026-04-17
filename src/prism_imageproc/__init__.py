1e415777a70bdc68f0660e0f460772fb7cfdca83
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2026-04-17 18:25:10 -0400

    Renamed to prism-imageproc

--- src/prism_imageproc/__init__.py
+++ src/prism_imageproc/__init__.py
@@ -1,7 +1,7 @@
 import importlib.metadata as metadata
 
 try:
-    __version__ = metadata.version(__package__ or 'mss_imageproc')
+    __version__ = metadata.version(__package__ or 'prism_imageproc')
 except metadata.PackageNotFoundError:
     # Fallback version if package metadata is not found. 
     # This should be updated if the version is changed in pyproject.toml.
